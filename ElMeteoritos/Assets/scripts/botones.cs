647fa7d84aa24cf00ade0f2f60f82b2f87cc4f8b
Javier <javier_pozuelo_m-tsapp1ma2324@cev.com>
2025-02-17 14:24:58 +0100

    conexiones iniciales con base de datos en la nueva web

--- ElMeteoritos/Assets/scripts/botones.cs
+++ ElMeteoritos/Assets/scripts/botones.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class botones : MonoBehaviour
 {
     public GameObject registro;
     public GameObject Login;
+
+    public string user, password, email;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,13 @@ public class botones : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    
+
+    public void conexionDB()
     {
         
     }
@@ -35,5 +45,29 @@ public class botones : MonoBehaviour
     {
         Login.SetActive(true);
         registro.SetActive(false);
+    }
+
+    public void Registrando()
+    {
+        StartCoroutine(llamadaTest());
+    }
+
+    public IEnumerator llamadaTest()
+    {
+        UnityWebRequest C = new UnityWebRequest();
+        WWWForm form = new WWWForm();
+        form.AddField("usuario", user);
+        form.AddField("password", password);
+        form.AddField("correo", email);
+        C = UnityWebRequest.Post("http://tfgmeteoroids.mygamesonline.org/test.php", form);
+        Debug.Log("lanzadito");
+        yield return C.SendWebRequest();
+        Debug.Log("lanzadito2");
+        while (!C.isDone)
+        {
+            Debug.Log("esperando...");
+            yield return null;
+        }
+        Debug.Log("mensaje: " + C.downloadHandler.text);
     }
 }
