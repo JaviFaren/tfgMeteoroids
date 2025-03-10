31403a63de9bf2be60bbea7acf906d2b9aca71e5
Lucas <lucas_macarro_m-tsapp1ma2324@cev.com>
2025-03-10 11:39:33 +0100

    Calcular bordes de camara y spawn de enemigos

--- ElMeteoritos/Assets/scripts/controlPartida.cs
+++ ElMeteoritos/Assets/scripts/controlPartida.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class controlPartida : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Debug")]
+    public bool bSpawnM = false;
+    public Camera cam;
+    public Vector3 bottomLeft;
+    public Vector3 topRight;
+    public float distance;
+    public GameObject meteoroidPrefab;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        distance = Mathf.Abs(cam.transform.position.z);
+
+        bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, distance));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (bSpawnM == true)
+        {
+            SpawnMeteoroid();
+            bSpawnM = false;
+        }
+    }
+
+    void SpawnMeteoroid()
+    {
+        float margin = 10f;
+        float xSpawn = 0, ySpawn = 0;
+        int side = Random.Range(0, 4);
+        Debug.Log("Side: " + side);
+
+        switch (side)
+        {
+            case 0: //Izquierda
+                xSpawn = bottomLeft.x - margin;
+                ySpawn = Random.Range(bottomLeft.y, topRight.y);
+                break;
+            case 1: //Derecha
+                xSpawn = topRight.x + margin;
+                ySpawn = Random.Range(bottomLeft.y, topRight.y);
+                break;
+            case 2: //Abajo
+                xSpawn = Random.Range(bottomLeft.x, topRight.x);
+                ySpawn = bottomLeft.y - margin;
+                break;
+            case 3: //Arriba
+                xSpawn = Random.Range(bottomLeft.x, topRight.x);
+                ySpawn = topRight.y + margin;
+                break;
+        }
+
+        Vector3 spawnPosition = new Vector3(xSpawn, ySpawn, 0);
+        Debug.Log(spawnPosition);
+        GameObject meteoroid = Instantiate(meteoroidPrefab, spawnPosition, Quaternion.identity);
+        meteoroid.GetComponent<detectarMeteorito>().SetTarget(Vector3.zero);
     }
 }
--- ElMeteoritos/Assets/scripts/detectarMeteorito.cs
+++ ElMeteoritos/Assets/scripts/detectarMeteorito.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class detectarMeteorito : MonoBehaviour
 {
+    public float speed = 3;
+    public Vector3 targetPosition;
+
+    [SerializeField] Rigidbody rb;
+
+    private void Start()
+    {
+        //rb2d =  GetComponent<Rigidbody>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "disparo")
         {
             Debug.Log("heh, me mato");
         }
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        targetPosition = target;
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector2 direction = (targetPosition - transform.position).normalized;
+        rb.velocity = direction * speed;
     }
 }
