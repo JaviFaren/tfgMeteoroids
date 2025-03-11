3e007973f105f9be962eff494beeae20c361adf7
Javier <javier_pozuelo_m-tsapp1ma2324@cev.com>
2025-03-11 12:41:04 +0100

    actualizado boton de disparo y codigo de numero de enemigos por oleada

--- ElMeteoritos/Assets/scripts/controlPartida.cs
+++ ElMeteoritos/Assets/scripts/controlPartida.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class controlPartida : MonoBehaviour
@@ -16,6 +17,10 @@ public class controlPartida : MonoBehaviour
     public Vector3 topRight;
     public float distance;
     public GameObject meteoroidPrefab;
+    public int oleada;
+    private bool nuevaOleada;
+    public TextMeshProUGUI oleadaText;
+    public int playerCount;
 
     private void Awake()
     {
@@ -37,6 +42,12 @@ public class controlPartida : MonoBehaviour
 
         bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, distance));
         topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, distance));
+
+        oleada = 1;
+        playerCount = 4;
+        nuevaOleada = true;
+
+        //StartCoroutine(prueba());
     }
 
     void Update()
@@ -46,9 +57,20 @@ public class controlPartida : MonoBehaviour
             SpawnMeteoroid();
             bSpawnM = false;
         }
+
+        //oleadaController();
     }
 
     void SpawnMeteoroid()
+    {
+
+        Vector3 spawnPosition = ChooseEnemySpawnPoint();
+        Debug.Log(spawnPosition);
+        GameObject meteoroid = Instantiate(meteoroidPrefab, spawnPosition, Quaternion.identity);
+        meteoroid.GetComponent<detectarMeteorito>().SetTarget(Vector3.zero);
+    }
+
+    Vector3 ChooseEnemySpawnPoint()
     {
         float margin = 10f;
         float xSpawn = 0, ySpawn = 0;
@@ -60,6 +82,7 @@ public class controlPartida : MonoBehaviour
             case 0: //Izquierda
                 xSpawn = bottomLeft.x - margin;
                 ySpawn = Random.Range(bottomLeft.y, topRight.y);
+                
                 break;
             case 1: //Derecha
                 xSpawn = topRight.x + margin;
@@ -73,11 +96,60 @@ public class controlPartida : MonoBehaviour
                 xSpawn = Random.Range(bottomLeft.x, topRight.x);
                 ySpawn = topRight.y + margin;
                 break;
+            default:
+                break;
+        }
+        return new Vector3(xSpawn, ySpawn, 0);
+    }
+
+    public void oleadaController()
+    {
+        if (nuevaOleada)
+        {
+            StartCoroutine(waveStarter());
+        }
+    }
+
+    public IEnumerator waveStarter()
+    {
+        oleadaText.gameObject.SetActive(true);
+        nuevaOleada = false;
+        string oleadaNum = "OLEADA " + oleada;
+        for(int i = 0; i < oleadaNum.Length; i++)
+        {
+            oleadaText.text = oleadaText.text + oleadaNum[i];
+            yield return new WaitForSeconds(0.3f);
+        }
+        yield return new WaitForSeconds(1);
+        oleadaText.text = "";
+        oleadaText.gameObject.SetActive(false);
+
+        startWave();
+    }
+    public int enemiesNum = 0;
+
+    public void startWave()
+    {
+        
+        int FactorJugadores = Mathf.RoundToInt(1 + (playerCount - 1) * 0.25f);
+        enemiesNum = Mathf.RoundToInt((3 + Mathf.Pow(oleada, 1.5f)) * FactorJugadores);
+        if (oleada == 1)
+        {
+            enemiesNum = 4 * playerCount;
+        }
+        else
+        {
+            enemiesNum = (Mathf.RoundToInt(oleada / 2) + 4) * playerCount;
         }
 
-        Vector3 spawnPosition = new Vector3(xSpawn, ySpawn, 0);
-        Debug.Log(spawnPosition);
-        GameObject meteoroid = Instantiate(meteoroidPrefab, spawnPosition, Quaternion.identity);
-        meteoroid.GetComponent<detectarMeteorito>().SetTarget(Vector3.zero);
+        //Debug.Log(enemiesNum);
     }
+
+    //public IEnumerator prueba()
+    //{
+    //    startWave();
+    //    yield return new WaitForSeconds(3);
+    //    oleada++;
+    //    StartCoroutine(prueba());
+    //}
 }
